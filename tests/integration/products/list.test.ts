f0890a37b47c30b80ccbd5bd1dfe3225b4df1ef9
Severino Vinicius <severino.sales95@gmail.com>
2023-09-13 21:36:46 -0300

    [Req 02] - Correcao dos testes

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -25,20 +25,34 @@ describe('GET /products', function () {
       }
     ]
 
-    sinon.stub(ProductModel, 'findAll').resolves([
-      ProductModel.build({
-        id: 1,
-        name: 'Excalibur',
-        price: '10 peças de ouro',
-        orderId: 1
-      }),
-      ProductModel.build({
-        id: 2,
-        name: 'Espada Justiceira',
-        price: '20 peças de ouro',
-        orderId: 1
-      }),
-    ])
+    sinon.stub(ProductModel, 'findAll').resolves(
+      ProductModel.bulkBuild([
+        {
+          id: 1,
+          name: 'Excalibur',
+          price: '10 peças de ouro',
+          orderId: 1
+        },
+        {
+          id: 2,
+          name: 'Espada Justiceira',
+          price: '20 peças de ouro',
+          orderId: 1
+        }
+      ])
+      // ProductModel.build({
+      //   id: 1,
+      //   name: 'Excalibur',
+      //   price: '10 peças de ouro',
+      //   orderId: 1
+      // }),
+      // ProductModel.build({
+      //   id: 2,
+      //   name: 'Espada Justiceira',
+      //   price: '20 peças de ouro',
+      //   orderId: 1
+      // }),
+    )
     const response = await chai.request(app).get('/products')
     expect(response.status).to.be.equal(200)
     expect(response.body).to.be.deep.equal(mockAllProducts)
